abdc2dd814c860083a7becc40cb2577920e0769a
skmajumder <skmajumder.cse@gmail.com>
2023-06-19 01:22:00 +0600

    callback then catch

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -3,18 +3,34 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+// fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
+//   console.log(`Breed: ${data}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
+
+//       console.log(res.body.message);
+//       fs.writeFile('./dog-img.txt', res.body.message, 'utf-8', (err) => {
+//         if (err) return console.log(err.message);
+//         console.log(`${data} dog breed image save to file`);
+//       });
+//     });
+// });
+
 fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
   console.log(`Breed: ${data}`);
-
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-
+    .then((res) => {
       console.log(res.body.message);
       fs.writeFile('./dog-img.txt', res.body.message, 'utf-8', (err) => {
         if (err) return console.log(err.message);
         console.log(`${data} dog breed image save to file`);
       });
+    })
+    .catch((err) => {
+      console.log(err.message);
     });
 });
