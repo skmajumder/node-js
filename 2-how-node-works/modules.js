3e10a7f57e2c615cc5cea12cd5a2bb4e8c586c06
skmajumder <skmajumder.cse@gmail.com>
2023-06-18 13:02:13 +0600

    Module

---
+++ 2-how-node-works/modules.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+const C = require('./test-module-1');
+const calc1 = new C();
+
+console.log(calc1.add(10, 20));
+
---
+++ 2-how-node-works/test-module-1.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// class Calculator {
+//   add(a, b) {
+//     return a + b;
+//   }
+
+//   multiply(a, b) {
+//     return a * b;
+//   }
+
+//   divide(a, b) {
+//     return a / b;
+//   }
+
+//   subtract(a, b) {
+//     return a - b;
+//   }
+// }
+
+module.exports = class {
+  add(a, b) {
+    return a + b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+};
