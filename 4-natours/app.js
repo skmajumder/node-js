36a376d8be200940405fde83deb4b317b8542e66
skmajumder <skmajumder.cse@gmail.com>
2023-06-24 03:04:13 +0600

    GET request and handle POST request

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,19 +1,54 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+
+// * Middleware
+app.use(express.json());
 
 // * PORT Number
 const PORT = process.env.PORT || 3000;
 
-// * Routes
+// * Read file (JSON)
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
 
-app.get('/', async (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side', app: 'Natours' });
+// * Routes
+app.get('/api/v1/tours', async (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    error: false,
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', async (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', async (req, res) => {
+  const newID = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newID }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    'utf-8',
+    (err) => {
+      if (err) {
+        return res.status(400).json({
+          status: 'failed',
+          error: true,
+        });
+      }
+      res.status(201).json({
+        status: 'success',
+        error: false,
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(PORT, () => {
