1aa363f82d177804109709dd9a9976c95972885a
skmajumder <skmajumder.cse@gmail.com>
2023-07-13 00:54:57 +0600

    MongoDB and mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successfull'));
 
 // * PORT Number
 const PORT = process.env.PORT || 3000;
