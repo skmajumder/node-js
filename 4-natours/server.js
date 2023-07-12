bb100859b5a7a7e0d5cd36dfecb506f79baf4412
skmajumder <skmajumder.cse@gmail.com>
2023-07-13 01:58:31 +0600

    Schema and model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -2,9 +2,12 @@ const dotenv = require('dotenv');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 
+const { Schema } = mongoose;
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+// * DB connection
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
@@ -15,9 +18,37 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log('DB connection successfull'));
+
+const tourSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Part Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log('Error 💥:', err));
 
 // * PORT Number
 const PORT = process.env.PORT || 3000;
