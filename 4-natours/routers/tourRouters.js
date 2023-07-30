04582bfc00f54be6ef606c50e0bb7e82ca64f5e3
skmajumder <skmajumder.cse@gmail.com>
2023-07-30 23:30:45 +0600

    Aggregation Pipeline Unwinding and Projecting

--- 4-natours/routers/tourRouters.js
+++ 4-natours/routers/tourRouters.js
@@ -10,6 +10,7 @@ router
   .get(tourController.aliesTopTour, tourController?.getAllTours);
 
 router.route('/tour-stats').get(tourController?.getTourStats);
+router.route('/monthly-plan/:year').get(tourController?.getMonthlyPlan);
 
 router
   .route('/')
